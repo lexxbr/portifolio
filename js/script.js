32ead6a8e1b0557d67796697d7c9eaaa5a841a8d
Alexandre Francisco de Souza - Analista QA <42357466+lexxbr@users.noreply.github.com>
2022-07-25 00:04:32 -0300

    Add files via upload
    atualizando

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /* abre ee fecha menu lateral em modo mobile */
-const menuMobile = document.querySelector('.menu-mobile')
-const body = document.querySelector('body')
+const menuMobile = document.querySelector(".menu-mobile")
+const body = document.querySelector("body")
 
 /* função callback + if ternario*/
-menuMobile,addEventListener('click', () => {
+menuMobile.addEventListener("click", () => {
     menuMobile.classList.contains("bi-list")
         ? menuMobile.classList.replace("bi-list", "bi-x")
         : menuMobile.classList.replace("bi-x", "bi-list");
@@ -11,14 +11,34 @@ menuMobile,addEventListener('click', () => {
 });
 
 /* Fechar o menu quando clicar em algum item */
-const navItem = document.querySelector('.navItem')
+const navItem = document.querySelectorAll(".nav-item");
 
-/* console.log(navItem) */
-navItem.forEach(item => {
-    item.addEventListener('click', () =>{
-        if (body.classList.contains("menu-nav-active")){
-           body.classList.remove("menu-nav-active") 
-           menuMobile.classList.replace("bi-x", "bi-list")
+/*console.log(navItem)*/
+navItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (body.classList.contains("menu-nav-active")) {
+        body.classList.remove("menu-nav-active");
+        menuMobile.classList.replace("bi-x", "bi-list");
+      }
+    });
+  });
+
+  const item = document.querySelectorAll("[data-anime]");
+
+  const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    /*console.log(windowTop)*/
+    item.forEach((element) => {
+        if(windowTop > element.offsetTop){
+            element.classList.add('animate')
+        } else {
+            element.classList.remove('animate');
         }
-    })
-})
+    });
+  };
+  animeScroll();
+
+  window.addEventListener("scroll", () =>{
+    animeScroll();
+  })
+  
